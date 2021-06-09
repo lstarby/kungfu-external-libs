a2d3ac5944863d8f1a33d5c78f0a5d4f5985991f
Keren DONG <keren.dong@kungfu.link>
2021-06-09 17:23:03 +0800

    Sync to s3

---
+++ lib.js
@@ -0,0 +1,7 @@
+const { spawn, spawnSync } = require("child_process");
+
+const spawnOptsInherit = { shell: true, stdio: "inherit", windowsHide: true };
+const spawnOptsPipe = { shell: true, stdio: "pipe", windowsHide: true };
+
+exports.list = function () {
+};
