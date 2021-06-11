9dca3fb97a6c2d2c2614d12c9d0e99144186d72b
Keren DONG <keren.dong@kungfu.link>
2021-06-11 16:39:20 +0800

    Build index.json

--- cli.js
+++ cli.js
@@ -1,6 +1,10 @@
-const { publish } = require("./lib.js");
+const { publish, build } = require("./lib.js");
 
 require("yargs/yargs")(process.argv.slice(2))
+    .command("build", "Build libs", (yargs) => {
+    }, (argv) => {
+        build();
+    })
     .command("list", "List libs", (yargs) => {
     }, (argv) => {
         console.log(fs.realpathSync(path.dirname(__dirname)));
--- lib.js
+++ lib.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
 const { spawn, spawnSync } = require("child_process");
 
 const spawnOptsInherit = { shell: true, stdio: "inherit", windowsHide: true };
@@ -5,3 +8,46 @@ const spawnOptsPipe = { shell: true, stdio: "pipe", windowsHide: true };
 
 exports.list = function () {
 };
+
+exports.build = function () {
+    let exlibs = {};
+    let exlibsDir = path.join(__dirname, "libs");
+    let buildArchImplMap = function (basePath, osImplMap, archName) {
+        let archPath = path.join(basePath, archName);
+        if (fs.existsSync(archPath)) {
+            let headers = [];
+            let bins = [];
+            glob.sync(path.join(archPath, "include", "*")).forEach(headerPath => {
+                headers.push(path.basename(headerPath));
+             });
+            glob.sync(path.join(archPath, "lib", "*")).forEach(binPath => {
+                bins.push(path.basename(binPath));
+            });
+            osImplMap[archName] = {
+                "include": headers,
+                "lib": bins
+            };
+        }
+    };
+    let buildOSImplMap = function (basePath, libMap, osName) {
+        if (fs.existsSync(path.join(basePath, osName))) {
+            let osImplMap = {};
+            buildArchImplMap(path.join(basePath, osName), osImplMap, "x86");
+            buildArchImplMap(path.join(basePath, osName), osImplMap, "x64");
+            libMap[osName] = osImplMap;
+        }
+    };
+    glob.sync(path.join(exlibsDir, "*")).forEach(libPath => {
+        exlibs[path.basename(libPath)] = {};
+        glob.sync(path.join(libPath, "*")).forEach(versionPath => {
+            let libName = path.basename(path.dirname(versionPath));
+            let libVersion = path.basename(versionPath);
+            let libMap = {};
+            buildOSImplMap(versionPath, libMap, "macos");
+            buildOSImplMap(versionPath, libMap, "linux");
+            buildOSImplMap(versionPath, libMap, "windows");
+            exlibs[libName][libVersion] = libMap;
+        });
+    });
+    fs.writeFileSync(path.join(exlibsDir, "index.json"), JSON.stringify(exlibs, null, 2));
+ };
